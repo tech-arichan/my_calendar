e0ebe54854446e1a274f0b5ea27245972c3fb125
tech-arichan <arihiro2940@gmail.com>
2021-01-11 11:23:25 +0900

    overall_schedule削除

--- app/models/overall_schedule.rb
+++
@@ -1,2 +0,0 @@
-class OverallSchedule < ApplicationRecord
-end
--- test/controllers/overall_schedules_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class OverallSchedulesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get overall_schedules_index_url
-    assert_response :success
-  end
-
-end
